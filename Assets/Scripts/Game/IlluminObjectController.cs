71645a2ef0e4ecf3af4a810a65c99577e043b4d9
kodfreedom <kodfreedom@gmail.com>
2018-11-12 18:37:51 +0900

    custom illumin shader

---
+++ Assets/Scripts/Game/IlluminObjectController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IlluminObjectController : MonoBehaviour
+{
+    [SerializeField] SphereCollider IlluminAwaker = null;
+    private Material material_ = null;
+
+	private void Start ()
+    {
+        var renderer = GetComponent<Renderer>();
+        if (renderer == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        material_ = renderer.material;
+	}
+	
+	private void Update ()
+    {
+        material_.SetVector("_IlluminAwakerPosition", IlluminAwaker.transform.position);
+        material_.SetFloat("_IlluminAwakerRadius", IlluminAwaker.radius);
+	}
+}
