55811946bfa334ca5869a2d731ada840aed7d79f
kodfreedom <kodfreedom@gmail.com>
2018-11-19 12:18:54 +0900

    fix conflict#2

--- Assets/Effects/EffectsScript/ChargingRightManager.cs
+++ Assets/Effects/EffectsScript/ChargingRightManager.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChargingRightManager : MonoBehaviour 
+public class ChargingRightManager : MonoBehaviour
 {
     public float CreateTime;
     private float TimeElapsed;
     public GameObject Effect;
-
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
         Create();
 
     }
-	
-	// Update is called once per frame
-	void Update () 
+
+    // Update is called once per frame
+    void Update()
     {
         TimeElapsed += Time.deltaTime;
 
@@ -29,9 +28,6 @@ public class ChargingRightManager : MonoBehaviour
 
     void Create()
     {
-        GameObject CloneObject = Instantiate(Effect, new Vector3(-1.0f, 0.0f, 0.0f), Quaternion.identity);
-        //CloneObject.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-        CloneObject.transform.parent = this.transform;
-        CloneObject.transform.position = this.transform.position;
+        Instantiate(Effect, Effect.transform.position, Effect.transform.rotation);
     }
 }
