4d3023f932ae098ee3a07737710acb5822c47445
kodfreedom <kodfreedom@gmail.com>
2018-11-21 19:21:08 +0900

    放電モードを消す

--- Assets/Scripts/Game/Player/PlayerMode/PlayerNormalMode.cs
+++ Assets/Scripts/Game/Player/PlayerMode/PlayerNormalMode.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerNormalMode : PlayerMode
 {
+    private bool playing_ultra_timeline_ = false;
+
     public override string Name()
     {
         return "PlayerNormalMode";
@@ -16,6 +18,8 @@ public class PlayerNormalMode : PlayerMode
 
     public override void Init(PlayerController player)
     {
+        GameManager.Instance.Data.MyInput.SetThunderMode(false);
+        playing_ultra_timeline_ = false;
     }
 
     public override void Uninit(PlayerController player)
@@ -25,22 +29,89 @@ public class PlayerNormalMode : PlayerMode
 
     public override void Update(PlayerController player)
     {
-        if (player.IsPlayingEvent) return;
+        player.UltraCollider.SetActive(player.EnableUltraCollider);
 
-        if (player.IsTunderMode == true)
+        if (playing_ultra_timeline_)
         {
-            var paramater = player.Parameter;
-            if(paramater.CurrentEnergy >= paramater.ThunderModeCost * Time.deltaTime)
+            if (player.UltraController.state == UnityEngine.Playables.PlayState.Paused)
             {
-                player.Change(player.ThunderMode);
-            }
-            else
-            {
-                GameManager.Instance.Data.MyInput.SetThunderMode(false);
+                playing_ultra_timeline_ = false;
+                player.IsPlayingEvent = false;
             }
             return;
         }
 
+        if (player.IsPlayingEvent) return;
+
+        // Punch
         base.UpdatePunch(player);
+
+        // Ultra
+        UpdateUltra(player);
+    }
+
+    private void UpdateUltra(PlayerController player)
+    {
+        var parameter = player.Parameter;
+        if (player.Ultra
+            && player.MyAnimator.GetFloat("EnableCharge") == 1f
+            && parameter.CurrentEnergy >= parameter.UltraCost)
+        {
+            if (GameManager.Instance.IsLastStage())
+            {
+                GameManager.Instance.StageClear();
+            }
+            else
+            {
+                StartUltra(player);
+            }
+        }
+    }
+
+    private void StartUltra(PlayerController player)
+    {
+        playing_ultra_timeline_ = true;
+        player.IsPlayingEvent = true;
+
+        // Set target
+        var target_group = player.Parameter.UltraTargetGroup;
+
+        if (player.CurrentNavigationState.Name() == player.BattleNavigationState.Name())
+        {
+            var enemies = player.BattleArea.Enemies;
+            int count = 0;
+            foreach (var enemy in enemies)
+            {
+                if (enemy.IsDead) continue;
+                enemy.OnExitFight();
+                ++count;
+            }
+
+            int target_number = count + 1;
+            float weight = 1f / target_number;
+            target_group.m_Targets = new Cinemachine.CinemachineTargetGroup.Target[enemies.Count + 1];
+
+            // player
+            target_group.m_Targets[0].target = player.transform;
+            target_group.m_Targets[0].weight = weight;
+
+            // enemy
+            count = 1;
+            foreach (var enemy in enemies)
+            {
+                if (enemy.IsDead) continue;
+                target_group.m_Targets[count].target = enemy.transform;
+                target_group.m_Targets[count].weight = weight;
+                ++count;
+            }
+        }
+        else
+        {
+            target_group.m_Targets[0].target = player.transform;
+            target_group.m_Targets[0].weight = 1f;
+        }
+
+        player.PunchCollider.SetActive(false);
+        player.UltraController.Play();
     }
 }
