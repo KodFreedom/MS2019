b3a9838184eb7825ce307cbde5963ad599f4241f
THS50177\murata.mitsuo <mitsuo.murata33@gmail.com>
2018-11-26 11:59:10 +0900

    ・電飾用のコードを追加した ・パンチを出やすくした

---
+++ Assets/Scripts/SerialHandler.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.Threading;
+
+public class SerialHandler
+{
+    public delegate void SerialDataReceivedEventHandler(string message);
+    public event SerialDataReceivedEventHandler OnDataReceived;
+
+    //ポート名
+    //例
+    //Linuxでは/dev/ttyUSB0
+    //windowsではCOM1
+    //Macでは/dev/tty.usbmodem1421など
+    public string portName = "COM3";
+    public int baudRate = 9600;
+
+    private SerialPort serialPort_;
+    private Thread thread_;
+    private bool isRunning_ = false;
+
+    private string message_;
+    private bool isNewMessageReceived_ = false;
+
+    public SerialHandler()
+    {
+        Open();
+        
+    }
+
+    public void Update()
+    {
+        if (isNewMessageReceived_)
+        {
+            OnDataReceived(message_);
+        }
+        isNewMessageReceived_ = false;
+    }
+
+    ~SerialHandler()
+    {
+        Close();
+    }
+
+    private void Open()
+    {
+        serialPort_ = new SerialPort(portName, baudRate, Parity.None, 8, StopBits.One);
+        //または
+        //serialPort_ = new SerialPort(portName, baudRate);
+        serialPort_.Open();
+
+        /*serialPort_.DtrEnable = true;
+        serialPort_.RtsEnable = true;*/
+
+        isRunning_ = true;
+
+        thread_ = new Thread(Read);
+        thread_.Start();
+    }
+
+    private void Close()
+    {
+        isNewMessageReceived_ = false;
+        isRunning_ = false;
+
+        if (thread_ != null && thread_.IsAlive)
+        {
+            thread_.Join();
+        }
+
+        if (serialPort_ != null && serialPort_.IsOpen)
+        {
+            serialPort_.Close();
+            serialPort_.Dispose();
+        }
+    }
+
+    private void Read()
+    {
+        /*while (isRunning_ && serialPort_ != null && serialPort_.IsOpen)
+        {
+            try
+            {
+                message_ = serialPort_.ReadLine();
+                isNewMessageReceived_ = true;
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }*/
+
+        int a = 0;
+    }
+
+    public void Write(string message)
+    {
+        try
+        {
+            serialPort_.Write(message);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
